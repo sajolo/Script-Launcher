4eb33093c22fd22699a8736c3d90cf0570fbc631
Saul Jorge <saul.jorge.lopez@gmail.com>
2024-12-04 13:03:24 +0100

    Añado correcciones al Script Launcher

--- script-launcher.py
+++ script-launcher.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import os
 import subprocess
+import shutil
 
 # --- Configuración general de estilos y colores en modo oscuro ---
 class DarkModeStyle:
@@ -84,30 +85,23 @@ class ScriptLauncherApp(tk.Tk):
     def show_aftermarket_scripts(self):
         """Mostrar los scripts disponibles en la carpeta Scripts-AFT con título, descripción y botón de ejecución"""
         scripts_info = [
-            {"title": "Dividir ficheros", "description": "El script descomprime archivos CSV desde //s02-ean/DataAcquisition/AFTERMARKET/_DIVIDIR_FICHEROS/FICHERO_ORIGINAL, "
-                                                             "divide cada CSV en 4 partes y guarda las partes en //s02-ean/DataAcquisition/AFTERMARKET/_DIVIDIR_FICHEROS.",
-             "script": "split_csv.py"},
-            {"title": "Unir ficheros", "description": "El script une las partes de archivos CSV ubicados en //s02-ean/DataAcquisition/AFTERMARKET/_DIVIDIR_FICHEROS y genera un "
-                                                          "archivo combinado con el prefijo joined_ en la misma carpeta.", "script": "join_csv.py"}
+            {"title": "Dividir ficheros", "description_file": "Instrucciones de uso del script split_csv.docx", "script": "split_csv.py"},
+            {"title": "Unir ficheros", "description_file": "Instrucciones de uso del script split_csv.docx", "script": "join_csv.py"}
         ]
         self.show_scripts_menu("C:/Users/Saul/Desktop/s02-ean/DataAcquisition/Script-Launcher/Scripts-formateo-datos/Scripts-AFT", scripts_info)
 
     def show_tarifas_scripts(self):
         """Mostrar los scripts disponibles en la carpeta Scripts-tarifas con título, descripción y botón de ejecución"""
         scripts_info = [
-            {"title": "Mercedes", "description": "El script descomprime archivos desde \\s02-ean\DataAcquisition\TARIFAS_ORIGINALES\_DESCARGAS\MERCEDES, "
-                                                     "procesa XML y genera un CSV y TXT en \\s02-ean\DataAcquisition\TARIFAS_ORIGINALES\MB1, \\s02-ean\DataAcquisition\TARIFAS_ORIGINALES"
-                                                     "\SM1, y \\s02-ean\DataAcquisition\TARIFAS_ORIGINALES\_DESATENDIDA\Pendientes.", "script": "mercedes_script.py"},
-            {"title": "Turquía", "description": "El script se conecta a un servidor FTP, descarga archivos TXT, los procesa generando archivos CSV en formato 1-2-3-4 "
-                                                     "y los guarda en \\s02-ean\DataAcquisition\TARIFAS_ORIGINALES\[brand_code]\TUR y \\s02-ean\DataAcquisition\TARIFAS_ORIGINALES\_DESATENDIDA"
-                                                     "\Pendientes.", "script": "script_turquia.py"},
-            {"title": "PSA", "description": "El script procesa archivos tar.gz para las marcas del grupo PSA y genera los CSV y TXT en lasrutas corespondientes.", "script": "script_psa.py"},
-            {"title": "Informe Tarifas", "description": "El script genera un informe XLSX (Excel) con datos de las tarifas que hay en el sistema en la ruta ?¿ y actualiza la base de datos con esos mismos datos.", "script": "script_tariff_report.py"}
+            {"title": "Mercedes", "description_file": "Documentación de proceso Mercedes.docx", "script": "mercedes_script.py"},
+            {"title": "Turquía", "description_file": "Documentación proceso script Turquía.docx", "script": "script_turquia.py"},
+            {"title": "PSA", "description_file": "Documentación proceso script PSA.docx", "script": "script_psa.py"},
+            {"title": "Informe Tarifas", "description_file": "Documentación proceso script Informes de Tarifas.docx", "script": "script_tariff_report.py"}
         ]
         self.show_scripts_menu("C:/Users/Saul/Desktop/s02-ean/DataAcquisition/Script-Launcher/Scripts-formateo-datos/Scripts-tarifas", scripts_info)
 
     def show_scripts_menu(self, folder_path, scripts_info):
-        """Mostrar los scripts disponibles en una carpeta con título y botón de ejecución, y un botón para ver la descripción en una ventana emergente"""
+        """Mostrar los scripts disponibles en una carpeta con título y botón de ejecución, y un botón para descargar la documentación"""
         for widget in self.container.winfo_children():
             widget.destroy()
 
@@ -117,7 +111,7 @@ class ScriptLauncherApp(tk.Tk):
         label = tk.Label(frame, text="Seleccione un script para ejecutar", font=(DarkModeStyle.FONT_FAMILY, 24), fg=DarkModeStyle.LABEL_COLOR, bg=DarkModeStyle.BG_COLOR)
         label.pack(pady=40)
 
-        # Mostrar cada script con título, botón de ver descripción y botón de ejecutar en filas de dos
+        # Mostrar cada script con título, botón de descargar documentación y botón de ejecutar en filas de dos
         row_frame = None
         for index, script_info in enumerate(scripts_info):
             if index % 2 == 0:
@@ -131,9 +125,9 @@ class ScriptLauncherApp(tk.Tk):
             title = tk.Label(script_frame, text=script_info["title"], font=(DarkModeStyle.FONT_FAMILY, 18, "bold"), fg=DarkModeStyle.LABEL_COLOR, bg=DarkModeStyle.BG_COLOR)
             title.pack(anchor="w", padx=10, pady=(10, 5))
 
-            # Botón para ver la descripción del script
-            description_button = self.create_rounded_button(script_frame, "Ver Descripción", lambda desc=script_info["description"]: self.show_description(desc), color=DarkModeStyle.BACK_BUTTON_COLOR)
-            description_button.pack(anchor="w", padx=10, pady=5)
+            # Botón para descargar la documentación del script
+            download_button = self.create_rounded_button(script_frame, "Descargar Documentación", lambda file=script_info["description_file"]: self.download_documentation(file), color=DarkModeStyle.BACK_BUTTON_COLOR)
+            download_button.pack(anchor="w", padx=10, pady=5)
 
             # Botón para ejecutar el script
             execute_button = self.create_rounded_button(script_frame, "Ejecutar", lambda s=script_info["script"]: self.run_script(folder_path, s))
@@ -143,16 +137,15 @@ class ScriptLauncherApp(tk.Tk):
         back_button = self.create_rounded_button(frame, "Volver", self.show_main_menu, color=DarkModeStyle.BACK_BUTTON_COLOR)
         back_button.pack(pady=40)
 
-    def show_description(self, description):
-        """Mostrar una ventana emergente con la descripción del script"""
-        description_window = tk.Toplevel(self)
-        description_window.title("Descripción del Script")
-        description_window.configure(bg=DarkModeStyle.BG_COLOR)
-        description_window.geometry("600x400")
-        description_text = tk.Text(description_window, wrap='word', font=(DarkModeStyle.FONT_FAMILY, 14), fg="#888888", bg=DarkModeStyle.BG_COLOR, relief="flat")
-        description_text.insert(tk.END, description)
-        description_text.config(state='disabled')
-        description_text.pack(expand=True, fill='both', padx=20, pady=20)
+    def download_documentation(self, filename):
+        """Descargar la documentación correspondiente al script"""
+        source_path = os.path.join("C:/Users/Saul/Desktop/s02-ean/DataAcquisition/Script-Launcher/documentation", filename)
+        destination_path = os.path.join(os.path.expanduser("~"), "Downloads", filename)
+        try:
+            shutil.copy(source_path, destination_path)
+            messagebox.showinfo("Descarga Completada", f"El archivo {filename} ha sido descargado en la carpeta de Descargas.")
+        except Exception as e:
+            messagebox.showerror("Error", f"Hubo un error al descargar el archivo {filename}.\n{str(e)}")
 
     def run_script(self, folder, script):
         """Ejecutar un script de Python abriendo una terminal PowerShell"""
@@ -162,7 +155,7 @@ class ScriptLauncherApp(tk.Tk):
             powershell_path = subprocess.check_output("where powershell", shell=True).decode().strip()
 
             # Ejecutar el script en PowerShell, abriendo la ventana de terminal y esperando el final
-            command = f'{powershell_path} -NoExit python "{script_path}"'
+            command = f'{powershell_path} -NoExit -Command "& {{ Start-Process python -ArgumentList \"{script_path}\" -Wait }}"'
             subprocess.Popen(command, shell=True)
         except Exception as e:
             messagebox.showerror("Error", f"Hubo un error al ejecutar el script {script}.\n{str(e)}")
